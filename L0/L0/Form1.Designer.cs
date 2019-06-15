8922487f4bf7285a5d34b4bf2ddf1f813029c6c1
Stepan <stepan-alekseev-87@mail.ru>
2019-06-15 17:02:30 +0700

    L0 - practice for summer02
    Former-commit-id: 5ccdda6daacff2e1935f2ead86bb5687a39fceff

--- L0/L0/Form1.Designer.cs
+++ L0/L0/Form1.Designer.cs
@@ -38,17 +38,30 @@
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -132,24 +145,6 @@
             this.label1.Text = "N";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(186, 39);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "M";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.Location = new System.Drawing.Point(225, 33);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown2.TabIndex = 4;
-            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(382, 29);
@@ -162,8 +157,9 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(15, 59);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 58);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(467, 278);
             this.dataGridView1.TabIndex = 6;
@@ -181,7 +177,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(610, 25);
+            this.button2.Location = new System.Drawing.Point(491, 29);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
@@ -189,11 +185,148 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "рефлексивность";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 389);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "антирефлексивность";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 415);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "симметричность";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 437);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "антисимметричность";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 467);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "транзитивность";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 496);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "label8";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "M";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(225, 25);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(720, 427);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Check";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 355);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(564, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(150, 382);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(564, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(150, 408);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(564, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(150, 434);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(564, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(150, 460);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(564, 20);
+            this.textBox5.TabIndex = 20;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(150, 489);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(564, 20);
+            this.textBox6.TabIndex = 21;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(807, 337);
+            this.ClientSize = new System.Drawing.Size(807, 521);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
@@ -209,9 +342,9 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,14 +361,26 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Button button2;
         public System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
         public System.Windows.Forms.NumericUpDown numericUpDown2;
-
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- L0/L0/Form1.cs
+++ L0/L0/Form1.cs
@@ -21,30 +21,18 @@ namespace L0
             InitializeComponent();
         }
 
-        private void toolStripTextBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("ФИО: Алексеев С.В. \nГруппа:ИП-712");
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+        public string[] elems = new string[18];
 
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-        public char[] arr  = new char [10];
-         
         private void button1_Click(object sender, EventArgs e)
         {
-            arr[0] = 'a'; arr[1] = 'b'; arr[2] = 'c'; arr[3] = 'd'; arr[4] = 'e'; arr[5] = 'f';
+            elems[0] = "a"; elems[1] = "b"; elems[2] = "c"; elems[3] = "d"; elems[4] = "e"; elems[5] = "f";
+            elems[6] = "g"; elems[7] = "h"; elems[8] = "i"; elems[9] = "j"; elems[10] = "k"; elems[11] = "l";
+            elems[12] = "m"; elems[13] = "n"; elems[14] = "o"; elems[15] = "p"; elems[16] = "q"; elems[17] = "r";
             n = Convert.ToInt32(numericUpDown1.Value);
 
             Random rand = new Random();
@@ -54,20 +42,20 @@ namespace L0
                 dataGridView1.ColumnCount = n;
                 dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
 
-                 mas = new int[n, n];
+                mas = new int[n, n];
                 int i, j;
                 for (i = 0; i < n; ++i)
                 {
-                    dataGridView1.Rows[i].HeaderCell.Value = arr[i].ToString();
+                    dataGridView1.Rows[i].HeaderCell.Value = elems[i].ToString();
                     for (j = 0; j < n; ++j)
                     {
-                        dataGridView1.Columns[j].HeaderCell.Value = arr[j].ToString();
+                        dataGridView1.Columns[j].HeaderCell.Value = elems[j].ToString();//называю хедеры колонок(столбцов) именами elems
                         mas[i, j] = rand.Next(2);
                         dataGridView1.Rows[i].Cells[j].Value = mas[i, j];
 
                     }
                 }
-                   
+
                 button2.Enabled = true;
                 maxToolStripMenuItem.Enabled = true;
             }
@@ -77,42 +65,111 @@ namespace L0
             }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            int[] mas = new int[n];
-            int max;
+            int[] masLocal = new int[n];
+            int max = 0;
             int i, j;
             for (i = 0; i < n; ++i)
             {
-                mas[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
                 for (j = 0; j < n; ++j)
                 {
-                    max = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
-                    if (max > mas[i])
-                        mas[i] = max;
+                    try
+                    {
+                        max = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                        masLocal[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+                    }
+                    catch (Exception ee)
+                    {
+                        MessageBox.Show("Значения ячеек м.б. только 1 и 0.\n" +
+                            "Проверьте ячейку [" + i + "][" + j + "] (" + elems[i] + ", " + elems[j] + ")", "неверный ввод",
+     MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+                    }
+
+                    if (max != 0 && max != 1)
+                    {
+                        MessageBox.Show("Значения ячеек м.б. только 1 и 0.\n В ячейке[" + i + "][" + j +
+                            "]  (" + elems[i] + ", " + elems[j] + ")" +
+                            "установлено: " + max, "неверный ввод",
+                         MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+                    }
+                    if (max > masLocal[i])
+                        masLocal[i] = max;
                 }
             }
             dataGridView2.RowCount = n;
             dataGridView2.ColumnCount = 1;
             for (i = 0; i < n; ++i)
-                dataGridView2.Rows[i].Cells[0].Value = mas[i];
+                dataGridView2.Rows[i].Cells[0].Value = masLocal[i];
             button2.Enabled = true;
             maxToolStripMenuItem.Enabled = true;//стрип меню - главное меню
         }
-
-        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
+            int i = 0, j = 0;
+            int[] arrCmp = new int[n];
 
+            for (i = 0; i < n; ++i)
+            {
+                for (j = 0; j < n; ++j)
+                {//двойная некрасивая проверка на правильность матрицы бинарного отношения
+                    try
+                    {
+                        mas[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                    catch (Exception ee)
+                    {
+                        MessageBox.Show("Значения ячеек м.б. только 1 и 0.\n" +
+                            "Проверьте ячейку [" + i + "][" + j + "] (" + elems[i] + ", " + elems[j] + ")", "неверный ввод",
+     MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+                    }
+
+                    if (mas[i, j] != 0 && mas[i, j] != 1)
+                    {
+                        MessageBox.Show("Значения ячеек м.б. только 1 и 0.\n В ячейке[" + i + "][" + j +
+                            "]  (" + elems[i] + ", " + elems[j] + ")" +
+                            "установлено: " + mas[i, j], "неверный ввод",
+                         MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+                    }
+                }
+            }
+            reflexivityCheck();
+            antireflexivityCheck();
+            symmetryCheck();
         }
-
-        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        public void reflexivityCheck()
         {
-
+            int cmp = 1;
+            for (int i = 0; i < n; i++) { cmp *= mas[i, i]; }
+            if (cmp == 1) textBox1.Text = "Отношение рефлексивно, т.к главаная диагональ состоит из единиц";
+            else textBox1.Text = "Отношение нерефлексивно, т.к главная диагональ не состоит из единиц";
+            button3.Enabled = true;
+        }
+        public void antireflexivityCheck()
+        {
+            int cmp = 0;
+            for(int i = 0; i<n; i++)
+            {
+                cmp += mas[i, i];
+                if (cmp == 0) textBox2.Text = "Отношение антирефлексивно, т.к. главная диагональ состоит из нулей";
+                else textBox2.Text = "Отношение не антирефлексивно, т.к главная диагональ не состоит из нулей";
+            }
+        }
+       public void symmetryCheck()
+        {
+            int i = 0, j = 0;
+            for (i = 0; i < n; i++) 
+                for(j = 0; j<n; j++)
+                {
+                    if (mas[i, j] != mas[j, i])
+                    {
+                        textBox3.Text = "Отношение не симметрично, т.к. (" + elems[i] + ", " + elems[j] +
+                            ") не равно (" + elems[j] + ", " + elems[i] + ")";
+                        return;
+                    }
+                    else continue;
+                }
+            textBox3.Text = "Отношение симметрично, т.к. элементы матрицы симметричны относительно главной диагонали";
         }
 
         private void sizeToolStripMenuItem_Click(object sender, EventArgs e)
@@ -122,36 +179,65 @@ namespace L0
             f.ShowDialog();
         }
 
-
-
-
-
         private void dataGridView1_CellValuePushed(object sender, DataGridViewCellValueEventArgs e)
-        {//здесь происходит интерактивное изменеие ячеек
-            int j = e.ColumnIndex;
-            int i = e.RowIndex;
-            mas[i, j] = Convert.ToInt32(e.ToString());
+        {//здесь происходит интерактивное изменеие ячеек... нет... эта штука пока не реагирует
+         /* int j = e.ColumnIndex;
+          int i = e.RowIndex;
+          int value = Convert.ToInt32(e.ToString());
+          if (value == 0 || value == 1)
+              mas[i, j] = value;
+          else
+          {
+              MessageBox.Show("your message",
+ "window title",
+ MessageBoxButtons.OK,
+ MessageBoxIcon.Warning // for Warning  
+                        //MessageBoxIcon.Error // for Error 
+                        //MessageBoxIcon.Information  // for Information
+                        //MessageBoxIcon.Question // for Question
+);
+          }*/
         }
         public void numericUpDown2_ValueChanged(object sender, EventArgs e)
         {
 
         }
-
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
 
         }
-
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {//этот метод - не то, что нужно  
 
         }
-
         public void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
 
         }
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
 
+        }
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
 
 
     }
