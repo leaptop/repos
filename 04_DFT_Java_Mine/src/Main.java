20223843f4f895579fd8a1145e3c392b385e34fb
Stepan <stepan-alekseev-87@mail.ru>
2019-07-18 02:17:01 +0700

    05_Java_Mine almost done

--- 04_DFT_Java_Mine/src/Main.java
+++ 04_DFT_Java_Mine/src/Main.java
@@ -51,8 +51,8 @@ public class Main {
                 sum.image += res_mul.image;
             }
             result[k].real = sum.real;//appointing the new numbers for the new resulting conplex numbers
-            result[k].image = sum.image;
-            if (result[k].image < 10e-10) result[k].image = 0;
+            result[k].image = sum.image;//yes, the backward transform is also complex for some reason
+           // if (result[k].image < 10e-10) result[k].image = 0;
             sum.real = 0;
             sum.image = 0;
         }
@@ -60,11 +60,11 @@ public class Main {
 
     public static void main(String[] args) {
         double array[] = new double[n];
-        ComplNum res[] = new ComplNum[n];//for A's in DFT
-        ComplNum source[] = new ComplNum[n];
+        ComplNum res[] = new ComplNum[n];//for A's in forward DFT (direct DFT outcome)
+        ComplNum result[] = new ComplNum[n];//for the backward DFT outcome
         for (int i = 0; i < n; i++) {
             res[i] = new ComplNum();
-            source[i] = new ComplNum();
+            result[i] = new ComplNum();
         }
         Random rand = new Random();
         System.out.println("Source array: ");
@@ -90,14 +90,14 @@ public class Main {
         System.out.println();
 
 
-        backwardsDFT(res, source);
+        backwardsDFT(res, result);
         System.out.println("\nReal part of back transform: ");
         for (int i = 0; i < n; i++) {
-            System.out.printf("%f ", source[i].real);
+            System.out.printf("%f ", result[i].real);
         }
         System.out.println("\nImage part of back transform: ");
         for (int i = 0; i < n; i++) {
-            System.out.printf("%f ", source[i].image);
+            System.out.printf("%f ", result[i].image);
         }
         System.out.println("\nDifficulty: " + count);
     }
